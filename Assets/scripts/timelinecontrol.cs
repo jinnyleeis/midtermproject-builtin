2f5f3adbd5be1b43c44549045b222abc0ab274e0
jinnyleeis <101055312+jinnyleeis@users.noreply.github.com>
2022-11-06 23:57:59 +0900

    zoom interaction

--- Assets/scripts/timelinecontrol.cs
+++ Assets/scripts/timelinecontrol.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
+using Cinemachine;
+
 
 public class timelinecontrol : MonoBehaviour
 {
+    [SerializeField] private PlayableDirector[] _playableDirectors;
     // Start is called before the first frame update
     void Start()
     {
         
+        PlayableDirector[] _playableDirectors = GetComponents<PlayableDirector>();
+        
+        _playableDirectors[0].Play();
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        
+    }
+
+    public void pd1()
+    {
+        _playableDirectors[1].Play();
     }
 }
