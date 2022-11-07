3c789037249abc129039ca6df2499f7b0e0f1668
jinnyleeis <101055312+jinnyleeis@users.noreply.github.com>
2022-11-07 14:07:03 +0900

    second interaction completed

---
+++ Assets/scripts/playerrotation.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerrotation : MonoBehaviour
+{
+
+
+    public Vector2 turn;
+
+    public float sensitivity = .5f;
+
+    public Vector3 deltaMove;
+
+    public float speed = 1;
+
+    public GameObject mover;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        turn.x += Input.GetAxis("Mouse X") * sensitivity;
+        turn.y += Input.GetAxis("Mouse Y") * sensitivity;
+        mover.transform.localRotation = Quaternion.Euler(0, turn.x, 0);
+        transform.localRotation = Quaternion.Euler(-turn.y, 0, 0);
+    }
+}
